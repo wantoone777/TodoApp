081f4e97977ffc3f7d35838c10825a59c8bb4676
wantoone[windows] <wantoone777@gmail.com>
2023-06-04 22:08:22 +0300

    to previous commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import todoApp from './components/TodoApp'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodoApp/TodoApp.js
@@ -0,0 +1,8 @@
+import TodoForm from '../TodoForm/TodoForm'
+import TodoList from '../TodoList/TodoList'
+
+function TodoApp() {
+	return <div></div>
+}
+
+export default TodoApp
